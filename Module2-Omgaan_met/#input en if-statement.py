4857741c722acd0e8f29c678dde9bee3747d03d2
Noa <99072699@mydavinci.nl>
2022-09-13 11:13:29 +0200

    Min en Max'

--- Module2-Omgaan_met/#input en if-statement.py
+++ Module2-Omgaan_met/#input en if-statement.py
@@ -6,12 +6,21 @@ b = int(input('Wat is b ='))
 
 if a > b :
     max = a
+    min = b
     print(f'a is het grootste getal : {max}')
+    print(f'Het maximum is {a}')
+    print(f'Het minimum is {b}')
+
 elif a < b :
     min = a
+    max = b
     print(f'a is het kleinste getal : {min}')
+    print(f'Het maximum is {b}')
+    print(f'Het minimum is {a}')
 else :
     print('a en b zijn even groot')
+
+
 
 
 
