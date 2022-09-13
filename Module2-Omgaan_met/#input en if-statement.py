257c95803b0e0ccd244e361549a852e59085f4e5
Noa <99072699@mydavinci.nl>
2022-09-13 10:30:23 +0200

    input en if-statement

---
+++ Module2-Omgaan_met/#input en if-statement.py
@@ -0,0 +1,10 @@
+#input en if-statement
+
+
+a = int(input('Wat is a ='))
+b = int(input('Wat is b ='))
+
+if a > b :
+    max = a
+    print(f'a is het grootste getal : {max}')
+
--- Module2-Omgaan_met/pizzaCalculator.py
+++ Module2-Omgaan_met/pizzaCalculator.py
@@ -6,16 +6,19 @@ print('Hoeveel pizza wilt u?')
 pizzal = int (input('Large 9.99 ='))
 pizzam = int (input('Medium 7.99 ='))
 pizzas = int (input('Small 5.99 ='))
+pizzami =int (input ('Mini 3.99 ='))
 
 prijsl = pizzal * 9.99
 prijsm = pizzam * 7.99
 prijss = pizzas * 5.99
+prijsmi = pizzami * 3.99
 
-totaal = prijsl + prijsm + prijss
+totaal = prijsl + prijsm + prijss + prijsmi
 
 print('---------------------------------------------')
 print(f'Pizza Large = {pizzal}  | {prijsl}')
 print(f'Pizza Medium = {pizzam} | {prijsm}')
 print(f'Pizza Small = {pizzas}  | {prijss}')
+print(f'Pizza Mini = {pizzami}  | {prijsmi}')
 print(f'Totale Prijs = {totaal} euro')
 print('---------------------------------------------')
