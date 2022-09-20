de638bd1b1c744448376b200d3eb2350b47148ac
Noa <99072699@mydavinci.nl>
2022-09-20 11:40:43 +0200

    kaas2

--- Module2-Omgaan_met/Kaas.py
+++ Module2-Omgaan_met/Kaas.py
@@ -1,42 +1,39 @@
 #Kaas.py
 
 print('Als je antwoord ja is type je 1 als je antwoord nee is type je 2')
-ant = int(input('Is de kaas geel?'))
+ant = input('Is de kaas geel?')
 
-#ja =1 nee =2
-ka = 1
-
-if ka == ant :
-    ant = int (input('Zitten er gaten in?'))
-    if ka == ant :
-        ant = int(input('Is de kaas belagelijk duur?'))
-        if ka == ant :
+if ant == 'ja':
+    ant2 = input('Zitten er gaten in?')
+    if ant2 == 'ja':
+        ant3 = input('Is de kaas belagelijk duur?')
+        if ant3 == 'ja':
             print('Emmenthaler')
-        elif ka < ant :
+        elif ant3 == 'nee':
             print('Leerdammer') 
             
-    elif ka < ant :
-     ant = int(input('Is de kaas hard als steen?'))
-     if ka == ant :
+    elif ant2 == 'nee':
+     ant4 = str(input('Is de kaas hard als steen?'))
+     if ant4 == 'ja':
             print('Parmigiano Reggiano')
-     elif ka < ant :
+     elif ant4 == 'nee':
         print('Goudse Kaas')
 
          
 
-elif ka < ant :
-    ant = int(input('Heeft de kaas blauwe schimmel?'))
-    if ka == ant :
-        ant = int(input('Heeft de kaas korst?'))
-        if ka == ant :
+elif ant == 'nee':
+    ant5 = input('Heeft de kaas blauwe schimmel?')
+    if ant5 == 'ja':
+        ant6 = input('Heeft de kaas korst?')
+        if ant6 == 'ja':
             print('Blue de Rochbaron')
-        elif ka < ant :
+        elif ant6 == 'nee':
             print('Foume d Ambert')
-    elif ka < ant :
-        ant = int(input('Heeft de kaas korst?'))
-        if ka == ant :
+    elif ant5 == 'nee':
+        ant7 = input('Heeft de kaas korst?')
+        if ant7 == 'ja':
             print('Camembert')
-        if ka < ant :
+        if ant7 == 'nee':
             print('Mozzarella')
      
 
