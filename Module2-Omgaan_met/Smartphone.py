efde788be8d75e05cb3753b99f153cc87399ba33
Noa <99072699@mydavinci.nl>
2022-09-22 10:32:49 +0200

    smartphone

--- Module2-Omgaan_met/Smartphone.py
+++ Module2-Omgaan_met/Smartphone.py
@@ -1,5 +1,6 @@
 prijs_iphone = int(input('Hoe duur is de iPone?'))
 prijs_samsung = int(input('Hoe duur is de Samsung?'))
+prijs_zenfone = int(input('Hoe duur is de Zenfone?'))
 
 if prijs_iphone > prijs_samsung:
     duurst = prijs_iphone
@@ -14,12 +15,17 @@ elif prijs_iphone < prijs_samsung:
     goedkoopst_telefoon = 'iPhone'
     
  
-if prijs_iphone < prijs_samsung or prijs_iphone > prijs_samsung:
+if (prijs_iphone >= 900) and (prijs_samsung >= 900):
+    print(f'De {duurst_telefoon} is het duurst, de telefoon kost : {duurst}')
+    print(f'De {goedkoopst_telefoon} is het goedkoopst, de telefoon kost : {goedkoopst}')
+    print('Het advies is dus geen telefoon te kopen, ze zijn te duur')
+
+elif prijs_iphone == prijs_samsung:
+    print('De iphone en samsung zijn even duur')
+
+elif prijs_iphone < prijs_samsung or prijs_iphone > prijs_samsung:
     verschil = duurst - goedkoopst    
     print(f'De {duurst_telefoon} is het duurst, de telefoon kost : {duurst}')
     print(f'De {goedkoopst_telefoon} is het goedkoopst, de telefoon kost : {goedkoopst}')
     print(f'Het advies is dus de {goedkoopst_telefoon} te kopen. Deze is namelijk {verschil} euro goedkoper dan de {duurst_telefoon}')
-elif prijs_iphone == prijs_samsung:
-    print('De iphone en samsung zijn even duur')
-
     
