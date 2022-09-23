846b5af21615211c5b77a29f47cec230bc5719e1
Noa <99072699@mydavinci.nl>
2022-09-23 09:28:54 +0200

    pizzacalculator2

--- Module2-Omgaan_met/pizzaCalculator.py
+++ Module2-Omgaan_met/pizzaCalculator.py
@@ -2,23 +2,27 @@
 #Noa Richardson opdracht: Pizza calculator
 
 print('Hoeveel pizza wilt u?')
+try:
+   pizzal = int (input('Large 9.99 ='))
+   pizzam = int (input('Medium 7.99 ='))
+   pizzas = int (input('Small 5.99 ='))
+   pizzami = int (input ('Mini 3.99 ='))
 
-pizzal = int (input('Large 9.99 ='))
-pizzam = int (input('Medium 7.99 ='))
-pizzas = int (input('Small 5.99 ='))
-pizzami =int (input ('Mini 3.99 ='))
+   prijsl = pizzal * 9.99
+   prijsm = pizzam * 7.99
+   prijss = pizzas * 5.99
+   prijsmi = pizzami * 3.99
 
-prijsl = pizzal * 9.99
-prijsm = pizzam * 7.99
-prijss = pizzas * 5.99
-prijsmi = pizzami * 3.99
+   totaal = prijsl + prijsm + prijss + prijsmi
 
-totaal = prijsl + prijsm + prijss + prijsmi
+   print('---------------------------------------------')
+   print(f'Pizza Large = {pizzal}  | {prijsl}')
+   print(f'Pizza Medium = {pizzam} | {prijsm}')
+   print(f'Pizza Small = {pizzas}  | {prijss}')
+   print(f'Pizza Mini = {pizzami}  | {prijsmi}')
+   print(f'Totale Prijs = {totaal} euro')
+   print('---------------------------------------------')
 
-print('---------------------------------------------')
-print(f'Pizza Large = {pizzal}  | {prijsl}')
-print(f'Pizza Medium = {pizzam} | {prijsm}')
-print(f'Pizza Small = {pizzas}  | {prijss}')
-print(f'Pizza Mini = {pizzami}  | {prijsmi}')
-print(f'Totale Prijs = {totaal} euro')
-print('---------------------------------------------')
+except:
+    print('U moet hele getallen invoeren')
+    
