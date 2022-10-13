d0763fbb5c36b7f69785ea778295514e3469c576
Noa <99072699@mydavinci.nl>
2022-10-13 10:20:11 +0200

    extra

--- Challenges/Challeges zwembad.py
+++ Challenges/Challeges zwembad.py
@@ -3,11 +3,21 @@ zwembad_breedte = float(input('Hoe breed is het zwembad in meters?'))
 zwembad_diepte = float(input('Hoe diep is het zwembad in meters?'))
 
 vierkante_meter = round(zwembad_lengte + zwembad_breedte, 2)
+if vierkante_meter < 20:
+    beton_m2 = 250
+else:
+    beton_m2 = 200
 inhoud_zwembad = round(zwembad_lengte * zwembad_breedte * zwembad_diepte, 2)
+
+if inhoud_zwembad < 20:
+    vaste_prijs = 100
+else:
+    vaste_prijs = 250
+
 kosten_uitgraven = round(inhoud_zwembad * 25.00, 2)
 kosten_afvoer = round(inhoud_zwembad * 32.50, 2)
-voorrijkosten = round(250 + (2.05 * 60), 2)
-beton_prijs = round(200 * vierkante_meter, 2)
+voorrijkosten = round(vaste_prijs + (2.05 * 60), 2)
+beton_prijs = round(beton_m2 * vierkante_meter, 2)
 totaal = round(kosten_uitgraven + kosten_afvoer + voorrijkosten, 2)
 
 print(f'Offerte voor een zwembad van {inhoud_zwembad} m3')
