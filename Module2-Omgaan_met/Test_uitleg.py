ae2f942b431fd363af5f2321c8581dde0896e7b6
Noa <99072699@mydavinci.nl>
2022-11-02 10:16:19 +0100

    test

--- Module2-Omgaan_met/Test_uitleg.py
+++ Module2-Omgaan_met/Test_uitleg.py
@@ -1,6 +1,15 @@
-#uitleg
+from random import choice, sample
+vorig_compliment = ""
 
-# start, stop ,stap grootte
-for x in range(0, 22, 2):
-    print(x)
+naam = input("Wat is je naam?")
+hoeveel_complimenten = int(input("Hoeveel complimenten wil je?"))
 
+compliment_lijst = (f"Je bent geweldig {naam}", f"Je bent goed bezig {naam}", f"Goed gedaan {naam}", f"Je ziet er goed uit {naam}")
+
+for x in range (hoeveel_complimenten):
+     compliment = choice(compliment_lijst)
+     while compliment == vorig_compliment:
+        compliment = choice(compliment_lijst)
+     print(compliment)
+     vorig_compliment = compliment
+     
