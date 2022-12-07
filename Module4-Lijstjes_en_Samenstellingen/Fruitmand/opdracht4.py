d2525ddb82bb5003f963497a1d37aa0cc77ebcad
Noa <99072699@mydavinci.nl>
2022-12-07 09:36:40 +0100

    fruitmand opdracht 4

--- Module4-Lijstjes_en_Samenstellingen/Fruitmand/opdracht4.py
+++ Module4-Lijstjes_en_Samenstellingen/Fruitmand/opdracht4.py
@@ -1,7 +1,8 @@
 from fruitmand import fruitmand
 import random
 
+
 aantal_fruit = int(input("Hoeveel stukken fruit?"))
 for x in range(aantal_fruit):
     random_fruit = random.randint(0, 6)
-    print(fruitmand[random_fruit])
+    print(fruitmand[random_fruit].get('name'))
