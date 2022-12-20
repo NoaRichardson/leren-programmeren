620096ca60abdd6bc866b3d49e2a6606c995811a
Noa <99072699@mydavinci.nl>
2022-12-20 10:25:19 +0100

    pseudo lootjes

---
+++ Module4-Lijstjes_en_Samenstellingen/pseudo_lootjes.py
@@ -0,0 +1,9 @@
+# geef een naam op
+# als de naam niet al is opgegeven voeg de naam toe in een namen lijst en een lootjes_lijst
+# als er meer dan 3 namen zijn vraag of je verder wilt
+# Als je niet meer wilt gaat die verder
+# zolang als er namen zijn:
+# neem een random naam van namen_lijst en lootjes_lijst
+# als ze niet het zelfde zijn doe je het in de dictionary
+
+# print dictionairy
