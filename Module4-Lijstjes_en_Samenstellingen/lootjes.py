bef9bccd1904bbba2846f0917d2d806ba0e88160
Noa <99072699@mydavinci.nl>
2022-12-21 09:58:34 +0100

    lootjes

--- Module4-Lijstjes_en_Samenstellingen/lootjes.py
+++ Module4-Lijstjes_en_Samenstellingen/lootjes.py
@@ -3,27 +3,35 @@ import random
 namen_list = []
 lootjes_list = []
 dictonairy = {}
-dictlist = []
+
 more_name = 'ja'
+while True:
+    while more_name == 'ja':
+        while True:
+            name = input('Naam?')
+            if name in namen_list:
+                print('U heeft deze naam al gekozen')
+            else:
+                break
+        
+        namen_list.append(name)
+        lootjes_list.append(name)
+        if len(namen_list) >= 3 :
+            more_name = input('Wilt u nog een naam toevoegen?')
 
-while more_name == 'ja':
-    name = input('Naam?')
-    if name in namen_list:
-        name = input('Naam?')
-    namen_list.append(name)
-    lootjes_list.append(name)
-    if len(namen_list) >= 3 :
-        more_name = input('Wilt u nog een naam toevoegen?')
 
-while len(namen_list) > 0:
-    naam = random.choice(namen_list)
-    lootje = random.choice(lootjes_list)
-    if naam != lootje:
-        dictonairy[naam] = lootje
-        namen_list.remove(naam)
-        lootjes_list.remove(lootje)
+    while len(namen_list) > 0:
+        naam = random.choice(namen_list)
+        lootje = random.choice(lootjes_list)
+        if naam != lootje:
+            dictonairy[naam] = lootje
+            namen_list.remove(naam)
+            lootjes_list.remove(lootje)
+            if len(namen_list) == 1 and len(lootjes_list) == 1:
+                break
+
 
 for x, y in dictonairy.items():
-    print(x, y)
+     print(x, y)
 
         
--- Module4-Lijstjes_en_Samenstellingen/pseudo_lootjes.py
+++ Module4-Lijstjes_en_Samenstellingen/pseudo_lootjes.py
@@ -1,9 +1,14 @@
-# geef een naam op
-# als de naam niet al is opgegeven voeg de naam toe in een namen lijst en een lootjes_lijst
-# als er meer dan 3 namen zijn vraag of je verder wilt
-# Als je niet meer wilt gaat die verder
+# herhaal
+    # geef een naam op
+    # als de naam niet al is opgegeven:
+        # voeg de naam toe in een namen lijst en een lootjes_lijst
+    # als er meer dan 3 namen zijn:
+        # vraag of je verder wilt
+    # Als je niet meer wilt gaat die verder met de code
+
 # zolang als er namen zijn:
-# neem een random naam van namen_lijst en lootjes_lijst
-# als ze niet het zelfde zijn doe je het in de dictionary
+    # neem een random naam van namen_lijst en lootjes_lijst
+    # als ze niet het zelfde zijn:
+        # doe je het in de dictionary
 
 # print dictionairy
