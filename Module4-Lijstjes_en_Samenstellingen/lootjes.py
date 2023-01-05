707aca78165bdfdd148e44650e3170135b68676a
Noa <99072699@mydavinci.nl>
2023-01-05 14:15:20 +0100

    lootjes

--- Module4-Lijstjes_en_Samenstellingen/lootjes.py
+++ Module4-Lijstjes_en_Samenstellingen/lootjes.py
@@ -3,35 +3,35 @@ import random
 namen_list = []
 lootjes_list = []
 dictonairy = {}
+last_name = ''
 
 more_name = 'ja'
-while True:
-    while more_name == 'ja':
-        while True:
-            name = input('Naam?')
-            if name in namen_list:
-                print('U heeft deze naam al gekozen')
-            else:
-                break
+
+while more_name == 'ja':
+    while True:
+        name = input('Naam?')
+        if name in namen_list:
+            print('U heeft deze naam al gekozen')
+        else:
+            break
         
-        namen_list.append(name)
-        lootjes_list.append(name)
-        if len(namen_list) >= 3 :
-            more_name = input('Wilt u nog een naam toevoegen?')
+    namen_list.append(name)
+    lootjes_list.append(name)
+    if len(namen_list) >= 3 :
+        more_name = input('Wilt u nog een naam toevoegen?')
 
 
-    while len(namen_list) > 0:
-        naam = random.choice(namen_list)
-        lootje = random.choice(lootjes_list)
-        if naam != lootje:
-            dictonairy[naam] = lootje
-            namen_list.remove(naam)
-            lootjes_list.remove(lootje)
-            if len(namen_list) == 1 and len(lootjes_list) == 1:
-                break
+while len(namen_list) > 0:
+    naam = random.choice(namen_list)
+    lootje = random.choice(lootjes_list)
+    if naam != lootje:
+        dictonairy[naam] = lootje
+        namen_list.remove(naam)
+        lootjes_list.remove(lootje)
+        if len(namen_list) == 0:
+            for x, y in dictonairy.items():
+                print(x, y)
 
 
-for x, y in dictonairy.items():
-     print(x, y)
 
-        
+       
