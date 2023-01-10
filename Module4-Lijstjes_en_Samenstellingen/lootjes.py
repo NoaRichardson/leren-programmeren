77f4653becc105d4ae8f3fd39034f7ee42210d8c
Noa <99072699@mydavinci.nl>
2023-01-10 13:21:31 +0100

    lootjes

--- Module4-Lijstjes_en_Samenstellingen/lootjes.py
+++ Module4-Lijstjes_en_Samenstellingen/lootjes.py
@@ -7,35 +7,32 @@ extra_list = []
 
 more_name = 'ja'
 
-# while more_name == 'ja':
-#     while True:
-#         name = input('Naam?')
-#         if name in namen_list:
-#             print('U heeft deze naam al gekozen')
-#         else:
-#             break
+while more_name == 'ja':
+    while True:
+        name = input('Naam?')
+        if name in namen_list:
+            print('U heeft deze naam al gekozen')
+        else:
+            break
         
-#     namen_list.append(name)
-#     lootjes_list.append(name)
-#     extra_list.append(name)
-#     if len(namen_list) >= 3 :
-#         more_name = input('Wilt u nog een naam toevoegen?')
-for x in range(100):
-    namen_list = ['jan', 'piet', 'klaas']
-    lootjes_list = ['jan', 'piet', 'klaas']
-    extra_list = ['jan', 'piet', 'klaas']
+    namen_list.append(name)
+    lootjes_list.append(name)
+    extra_list.append(name)
+    if len(namen_list) >= 3 :
+        more_name = input('Wilt u nog een naam toevoegen?')
+
+while len(namen_list) > 0:
+    namen_list = [] + extra_list
+    lootjes_list = [] + extra_list
     while len(namen_list) > 0:
-        namen_list = [] + extra_list
-        lootjes_list = [] + extra_list
-        while len(namen_list) > 0:
-            naam = random.choice(namen_list)
-            lootje = random.choice(lootjes_list)
-            if naam != lootje:
-                lootjes_dict[naam] = lootje
-                namen_list.remove(naam)
-                lootjes_list.remove(lootje)
-                if len(namen_list) == 0:
-                    for x, y in lootjes_dict.items():
-                        print(x, y)
-            elif naam == lootje:
-                break
+        naam = random.choice(namen_list)
+        lootje = random.choice(lootjes_list)
+        if naam != lootje:
+            lootjes_dict[naam] = lootje
+            namen_list.remove(naam)
+            lootjes_list.remove(lootje)
+            if len(namen_list) == 0:
+                for x, y in lootjes_dict.items():
+                    print(x, y)
+        elif naam == lootje:
+            break
