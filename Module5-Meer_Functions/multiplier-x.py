f0f59674bf5cd6deaf896f1b064864c50b076cf2
Noa <99072699@mydavinci.nl>
2023-01-12 10:21:27 +0100

    keer som

---
+++ Module5-Meer_Functions/multiplier-x.py
@@ -0,0 +1,10 @@
+def keer_som(vraag: int):
+    tafel = ""
+    for x in range(1,11):
+        tafel += f'{x} x {nummer} = {x * nummer}\n'
+    return tafel
+
+nummer = int(input("Welke tafel wilt u zien?"))
+print(keer_som(nummer))
+
+
