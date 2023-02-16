bc13318a0dae59708d91a46c15b18c28769cb035
Noa <99072699@mydavinci.nl>
2023-02-16 11:18:52 +0100

    bootstrap

---
+++ Dynamische_User_Interfaces/cijfer_ruit.js
@@ -0,0 +1,12 @@
+let getal = prompt("Kies een getal");
+var ruit;
+var index = 0;
+for(let i = 1; i <= getal; i ++){
+    process.stdout.write('')
+    }
+    for(let x = 0; x <= getal * x - 1; x ++){
+        process.stdout.write(getal[index])
+        index =+ 1
+    }
+
+console.log(ruit)
