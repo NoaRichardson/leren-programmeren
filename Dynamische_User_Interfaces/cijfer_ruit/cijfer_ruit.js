941ca8550e58e779f4eb250a8966bef06dc78f8b
Noa <99072699@mydavinci.nl>
2023-03-09 10:19:05 +0100

    cijfer_ruit eindelijk

--- Dynamische_User_Interfaces/cijfer_ruit/cijfer_ruit.js
+++ Dynamische_User_Interfaces/cijfer_ruit/cijfer_ruit.js
@@ -1,16 +1,14 @@
 getal = prompt("Kies een getal");
 antw = document.getElementById("antwoord");
-tot = ""
-tot_2 = ""
+const tot = []
+ruit = ""
 
 for(let y = 1; y <= getal; y ++){   
-    tot += y;
-    document.write(tot + "<br>")    
+    ruit += y + "-"
+    document.write(ruit.slice(0, -1)+ "<br>");
+    tot.push(ruit.slice(0,-1)+ "<br>");   
 }
 
-for(let i = getal; i >= 1; i --){
-    for(let j = 1; j <= i; i++){
-        tot_2 += j;
-    }
-    document.write(tot_2 + "<br>")
+for(let i = getal; i >= 2; i --){
+    document.write(tot[i-2]);
 }
