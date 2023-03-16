7e6d80bf64abdb983c5dbb27cf821dc413b5625d
Noa <99072699@mydavinci.nl>
2023-03-16 11:36:29 +0100

    M05.D02.02

--- Module5-Meer_Functions/treasure_hunt/functions.py
+++ Module5-Meer_Functions/treasure_hunt/functions.py
@@ -5,19 +5,19 @@ from data import JOURNEY_IN_DAYS
 ##################### M04.D02.O2 #####################
 
 def copper2silver(amount:int) -> float:
-    pass
+    return amount / 10
 
 def silver2gold(amount:int) -> float:
-    pass
+    return amount / 5
 
 def copper2gold(amount:int) -> float:
-    pass
+    return silver2gold(copper2silver(amount))
 
 def platinum2gold(amount:int) -> float:
-    pass
+    return amount * 25
 
 def getPersonCashInGold(personCash:dict) -> float:
-    pass
+    return copper2gold(personCash['copper']) + silver2gold(personCash['silver']) + personCash['gold'] + platinum2gold(personCash['platinum'])
 
 ##################### M04.D02.O4 #####################
 
