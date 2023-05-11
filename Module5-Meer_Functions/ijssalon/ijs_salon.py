a858e49efd104b74100cc370a92390e90cc61d15
Noa <99072699@mydavinci.nl>
2023-05-11 15:27:10 +0200

    ijssalon BTW

--- Module5-Meer_Functions/ijssalon/ijs_salon.py
+++ Module5-Meer_Functions/ijssalon/ijs_salon.py
@@ -1,35 +1,48 @@
 from salon_functions import *
 
+order = ""
+zaken_order = ""
+lijst_smaken_liters = [0, 0, 0, 0]
+prijs_liter = 9.80
 aantal_hoorntjes = 0
 aantal_bakjes = 0
 prijs_topping = 0
 smaken = []
-print("Welkom bij Papi Gelato.")
+klant = wat_voor_klant()
+if klant == "1":
+    print("Welkom bij Papi Gelato.")
+    order = "ja"
+else:
+    zaken_order = "ja"
 
-order = "ja"
 while order == "ja":
     bollentjes = hoeveel_bollentjes()
     if bollentjes > 8:
         print("Sorry we hebben geen bakjes dat groot.")
     else:
-        smaken += smaak_bollentjes()
+        smaken += wat_voor_smaak(bollentjes)
         if bollentjes < 4:
             ijs_houder = vraag_hoorntje_bakje()
-            if ijs_houder == "hoorntje":
-                aantal_hoorntjes += 1
-                ijs_houder = "hoorntje"
-            else:
-                aantal_bakjes += 1
-                ijs_houder = "bakje"
-        elif bollentjes >= 4 and bollentjes <= 8:
-            aantal_bakjes += 1
+        else:
             ijs_houder = "bakje"
+
+        if ijs_houder == "hoorntje":
+            aantal_hoorntjes += 1
+        else:
+            aantal_bakjes += 1
 
         bollentje_topping = topping()
         prijs_topping += berekenen_topping(bollentje_topping, bollentjes, ijs_houder)
         order = meer_bestellen(ijs_houder)
 
+while zaken_order == "ja":
+    liters = hoeveel_liter()
+    smaak_liters = wat_voor_smaak(liters)
+    lijst_smaken_liters = aantal_smaken(smaak_liters)
+    zaken_order = "nee"
+
+
 hoorntjes_bakjes = aantal_hoorntjes_bakjes(aantal_hoorntjes, aantal_bakjes)       
 aantal_bollentjes_smaken = aantal_smaken(smaken)
-bon = berekenen_bon(hoorntjes_bakjes, aantal_bollentjes_smaken, aantal_hoorntjes, aantal_bakjes, prijs_topping)
+bon = berekenen_bon(hoorntjes_bakjes, aantal_bollentjes_smaken, aantal_hoorntjes, aantal_bakjes, prijs_topping, lijst_smaken_liters, zaken_order)
 print(bon)
--- Module5-Meer_Functions/ijssalon/salon_functions.py
+++ Module5-Meer_Functions/ijssalon/salon_functions.py
@@ -1,7 +1,7 @@
 def hoeveel_bollentjes():
     while True:
         hoeveel = input("Hoeveel bollentjes wilt u?")
-        if hoeveel.isnumeric():
+        if hoeveel.isnumeric() and int(hoeveel) > 0:
             return int(hoeveel)
         else:
             print("Sorry dat snap ik niet...")
@@ -28,29 +28,32 @@ def aantal_hoorntjes_bakjes(aantal_hoorntjes, aantal_bakjes):
      prijs_bakjes = round(aantal_bakjes * 0.75, 2)
      return prijs_hoorntjes, prijs_bakjes
 
-def smaak_bollentjes():
+def wat_voor_smaak(bollentjes):
+    lijst_smaak = []
     while True:
-        smaak = input("Welke smaak wilt u voor bollentje?\nA)Aardbei\nC)Chocolade\nM)Munt\nV(Vanille\nSmaak:")
-        if smaak not in ("a", "c", "m", "v"):
-            print("Sorry dat snap ik niet...")
-        else:
-            return smaak
+        for ijs in range(0, bollentjes):
+            smaak = input("Welke smaak wilt u?\nA)Aardbei\nC)Chocolade\nM)Munt\nV)Vanille\nSmaak:")
+            if smaak not in ("a", "c", "m", "v"):
+                print("Sorry dat snap ik niet...")
+            else:
+                lijst_smaak.append(smaak)
+        return lijst_smaak
 
 def aantal_smaken(smaken):
-    aarbei_bollentjes = 0
-    chocolade_bollentjes = 0
-    munt_bollentjes = 0
-    vanilla_bollentjes = 0
+    aarbei = 0
+    chocolade = 0
+    munt = 0
+    vanilla = 0
     for i in smaken:
         if i == "a":
-            aarbei_bollentjes += 1
+            aarbei += 1
         elif i == "c":
-            chocolade_bollentjes += 1
+            chocolade += 1
         elif i == "m":
-            munt_bollentjes += 1
+            munt += 1
         elif i == "v":
-            vanilla_bollentjes += 1
-    return aarbei_bollentjes, chocolade_bollentjes, munt_bollentjes, vanilla_bollentjes
+            vanilla += 1
+    return aarbei, chocolade, munt, vanilla
 
 def topping():
     while True:
@@ -75,7 +78,7 @@ def berekenen_topping(bollentje_topping, bollentjes, ijs_houder):
     return prijs_topping
 
 
-def berekenen_bon(hoorntjes_bakjes, aantal_bollentjes_smaken, aantal_hoorntjes, aantal_bakjes, prijs_topping):
+def berekenen_bon(hoorntjes_bakjes, aantal_bollentjes_smaken, aantal_hoorntjes, aantal_bakjes, prijs_topping, lijst_smaken_liters, zaken_order):
     totaal = 0
     bon = "---------Papi Gelato---------\n"
     if aantal_bollentjes_smaken[0] > 0:
@@ -95,13 +98,45 @@ def berekenen_bon(hoorntjes_bakjes, aantal_bollentjes_smaken, aantal_hoorntjes, 
         bon += f"B.Vanille    {aantal_bollentjes_smaken[3]} x $1,10 = {v_totaal}\n"
         totaal += v_totaal
     if aantal_hoorntjes > 0:
-        bon += f"Hoorntje     {aantal_hoorntjes} x 1.25 = {hoorntjes_bakjes[0]}\n"
+        bon += f"Hoorntje     {aantal_hoorntjes} x $1.25 = {hoorntjes_bakjes[0]}\n"
         totaal += hoorntjes_bakjes[0]
     if aantal_bakjes > 0:
-        bon += f"Bakjes       {aantal_bakjes} x 0.75 = {hoorntjes_bakjes[1]}\n"
+        bon += f"Bakjes       {aantal_bakjes} x &0.75 = {hoorntjes_bakjes[1]}\n"
         totaal += hoorntjes_bakjes[1]
     if prijs_topping > 0:
         bon += f"Topping                 {prijs_topping}\n"
         totaal += prijs_topping
-    bon += f"Totaal                  {round(totaal, 2)}"
+    # Berekeningen voor zaken klanten.
+    if lijst_smaken_liters[0] > 0:
+        bon += f"L.Aardbei      {lijst_smaken_liters[0]} x 9.80 = {round(lijst_smaken_liters[0] * 9.80, 2)}\n"
+        totaal += lijst_smaken_liters[0] * 9.80
+    if lijst_smaken_liters[1] > 0:
+        bon += f"L.Chocolade    {lijst_smaken_liters[1]} x 9.80 = {round(lijst_smaken_liters[1] * 9.80, 2)}\n"
+        totaal += lijst_smaken_liters[1] * 9.80
+    if lijst_smaken_liters[2] > 0:
+        bon += f"L.Munt         {lijst_smaken_liters[2]} x 9.80 = {round(lijst_smaken_liters[2] * 9.80, 2)}\n"
+        totaal += lijst_smaken_liters[2] * 9.80
+    if lijst_smaken_liters[3] > 0:
+        bon += f"L.Vanilla      {lijst_smaken_liters[3]} x 9.80 = {round(lijst_smaken_liters[3] * 9.80, 2)}\n"
+        totaal += lijst_smaken_liters[3] * 9.80
+
+    bon += f"Totaal                  {round(totaal, 2)}\n"
+    if zaken_order == "nee":
+        bon += f"BTW(9%)                 {round(totaal/100 * 9, 2)}"
     return bon
+
+def wat_voor_klant():
+    while True:
+        kies_wat_voor_klant = input("Bent u 1) een particuliere klant of 2) een zakelijke klant?")
+        if kies_wat_voor_klant not in ("1", "2"):
+            print("Sorry dat snap ik niet...")
+        else:
+            return kies_wat_voor_klant
+    
+def hoeveel_liter():
+    while True:
+        hoeveelheid_liter = input("Hoeveel liter wilt u?")
+        if hoeveelheid_liter.isnumeric() and int(hoeveelheid_liter) > 0:
+            return int(hoeveelheid_liter)
+        else:
+            print("Sorry dat snap ik niet...")
