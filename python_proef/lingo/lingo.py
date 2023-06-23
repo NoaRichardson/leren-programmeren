077e45c634ce7fd545f7da43f6e2a5251459611d
Noa <99072699@mydavinci.nl>
2023-06-23 11:47:23 +0200

    lingo almost done'

--- python_proef/lingo/lingo.py
+++ python_proef/lingo/lingo.py
@@ -3,14 +3,19 @@ from lingo_function import *
 import random
 
 word = random.choice(words)
-word = "aabbc"
 poging = 5
-hoeveel_letter = f"{word[0]} _ _ _ _"
+hoeveel_letter = f"{word[0]}____"
+test = [f"{word[0]}", "_", "_", "_", "_"]
 print(hoeveel_letter)
 print(word)
-result_string = ["_", "_", "_", "_", "_"]
-
-# print(''.join(result_try))
-
-# if result_try != word:
-#     print(word)
+for x in range(poging):
+    guess = try_word()
+    green = find_green(word, guess)
+    test_list = remove_green(green, word)
+    yellow = find_yellow(test_list, guess)
+    result_guess = print_guess(green, yellow, guess)
+    # guessed_right = bijhouden_guess(result_guess, test)
+    # test = result_guess
+    print("")
+    # print(''.join(result_guess))
+# Fix zodat hij je guesses bijhoud
--- python_proef/lingo/lingo_function.py
+++ python_proef/lingo/lingo_function.py
@@ -9,3 +9,79 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# 1 Find the green and remove from copy list and put in other list
+# 2 Find yellow remove from copy list put with green in order
+# 3 Print the colors right
+
+def try_word():
+    while True:
+        guess = input("Raad:")
+        if len(guess) > 5:
+            print("Alle woorden zijn maar 5 letters lang")
+        elif len(guess) < 5:
+            print("Alle woorden zijn 5 letters lang")
+        else:
+            return guess
+        
+def find_green(word, guess):
+    list_green = []
+    index = 0
+    for letter in word:
+        if guess[index] == word[index]:
+            list_green.append(letter)
+            index += 1
+        else:
+            list_green.append("_")
+            index += 1
+    return list_green
+
+def remove_green(green, word):
+    list_no_green = []
+    index = 0
+    for letter in word:
+        if letter == green[index]:
+            list_no_green.append("_")
+            index += 1
+        else:
+            list_no_green.append(letter)
+            index += 1
+    return list_no_green
+
+def find_yellow(test_list, guess):
+    yellow = []
+    for letter in guess:
+        if letter in test_list:
+            yellow.append(letter)
+            test_list.remove(letter)
+        else:
+            yellow.append("_")
+    return yellow
+
+def print_guess(green, yellow, guess):
+    index = 0
+    result_guess = []
+    for letter in guess:
+        if letter == green[index]:
+            print(bcolors.OKGREEN + f"{letter}" + bcolors.ENDC, end="")
+            result_guess.append(letter)
+            index += 1
+        elif letter == yellow[index]:
+            print(bcolors.WARNING + f"{letter}" + bcolors.ENDC, end="")
+            result_guess.append("_")
+            index += 1
+        else:
+            print("_", end="")
+            result_guess.append("_")
+            index += 1
+    return result_guess
+
+def bijhouden_guess(result_guess, test):
+    guessed = []
+    index = 0
+    for letter in test:
+        if letter != "_":
+            guessed.append(letter)
+            index += 1
+        elif result_guess[index] != "_":
+            guessed.append(result_guess[index])
+    return guessed
