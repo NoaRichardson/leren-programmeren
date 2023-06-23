6759d880faf7481ee6726a27f9e0d1a05dc27a6f
Noa <99072699@mydavinci.nl>
2023-06-23 09:15:58 +0200

    htdoc update

--- python_proef/lingo/lingo.py
+++ python_proef/lingo/lingo.py
@@ -3,21 +3,14 @@ from lingo_function import *
 import random
 
 word = random.choice(words)
+word = "aabbc"
 poging = 5
 hoeveel_letter = f"{word[0]} _ _ _ _"
 print(hoeveel_letter)
 print(word)
 result_string = ["_", "_", "_", "_", "_"]
-for x in range(poging):
-    try_word = poging_raden(word)
-    result_try = hoeveel_juist(try_word, word)
-    # for position in range(len(result_try)):
-    #     if result_try[position] != "_":
-    #         result_string[position] = result_try[position]
-    print (f"\n{''.join(result_try)}")
-    if result_try == word:
-        print(bcolors.OKGREEN + "Je hebt het geraden!" + bcolors.ENDC)
-        break
 
-if result_try != word:
-    print(word)
+# print(''.join(result_try))
+
+# if result_try != word:
+#     print(word)
