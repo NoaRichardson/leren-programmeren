90bf8ed2f85e230ed795b6a8b666686a1b6f27aa
Noa <noa@intermixmanagement.nl>
2024-01-29 11:26:53 +0100

    button-3 finished

--- Dynamische_User_Interfaces/buttons/buttons-3/buttons-3.js
+++ Dynamische_User_Interfaces/buttons/buttons-3/buttons-3.js
@@ -1,10 +1,10 @@
-buttonAmount = 30;
+const buttonAmount = 30;
 
 //Create buttons
 for(let buttons = 1; buttons < buttonAmount + 1; buttons++){
     let button = document.createElement("button");
     button.innerHTML = buttons;
-    button.classList.add("button")
+    button.classList.add(`button${buttons}`)
     const container = document.getElementById("container");
     container.appendChild(button);
     let counter = 0
@@ -12,14 +12,28 @@ for(let buttons = 1; buttons < buttonAmount + 1; buttons++){
     //Create event for 'click'
     button.addEventListener('click', () => {
         counter++;
-        colorChange(counter)
+        colorChange(counter, buttons)
     })
 }
 
 //Function change the color by 'click'
-function colorChange(counter) {
+function colorChange(counter, buttons) {
     switch(counter) {
         case 1:
-            document.getElementsByClassName("button").style.backgroundColor = "#FF0000";//--Try array with objects keeping the counter by to solve
+            document.getElementsByClassName(`button${buttons}`)[0].style.backgroundColor = "#FF0000";
+            break;
+        case 2:
+            document.getElementsByClassName(`button${buttons}`)[0].style.backgroundColor = "#A020F0";
+            break;
+        case 3:
+            document.getElementsByClassName(`button${buttons}`)[0].style.backgroundColor = "#00008B";
+            break;
+        case 4:
+            document.getElementsByClassName(`button${buttons}`)[0].style.backgroundColor = "#000000";
+            break;
+        case 5:
+            let button = document.getElementsByClassName(`button${buttons}`)[0];
+            document.getElementById("container").removeChild(button);
+            break;
     }
 }
